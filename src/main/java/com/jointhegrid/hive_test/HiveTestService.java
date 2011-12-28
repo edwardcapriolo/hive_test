8db513be791efeba0164e9bbd47f647eca8cc513
Edward Capriolo <edlinuxguru@gmail.com>
2011-12-28 18:40:04 -0500

    Sevice Hive added

--- src/main/java/com/jointhegrid/hive_test/HiveTestService.java
+++ src/main/java/com/jointhegrid/hive_test/HiveTestService.java
@@ -1,4 +1,5 @@
 package com.jointhegrid.hive_test;
+
 import com.jointhegrid.hive_test.HiveTestBase;
 import java.io.IOException;
 
@@ -14,58 +15,56 @@ import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
-
 public abstract class HiveTestService extends HiveTestBase {
 
-	protected String host;
-	protected int port;
-	protected TTransport transport;
-	protected HiveInterface client;
-	protected boolean standAloneServer = false;
-	
-	public HiveTestService() throws IOException {
-		super();
-		host = "localhost";
-		port = 10000;
-	}
-	
-	public void setUp() throws Exception {	
-		super.setUp();
-		Path rootDir = getDir(ROOT_DIR);
-		Configuration conf = createJobConf();
-		FileSystem fs = FileSystem.get(conf);
-		fs.delete(rootDir, true);
-		Path metastorePath = new Path("/tmp/metastore_db");
-		fs.delete(metastorePath, true);
-		Path warehouse = new Path("/tmp/warehouse");
-		fs.delete(warehouse, true);
-		fs.mkdirs(warehouse);
+  protected String host;
+  protected int port;
+  protected TTransport transport;
+  protected HiveInterface client;
+  protected boolean standAloneServer = false;
 
-		if (standAloneServer) {
-			try {
-				transport = new TSocket(host, port);
-				TProtocol protocol = new TBinaryProtocol(transport);
-				client = new HiveClient(protocol);
-				transport.open();
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
-		} else {
-			client = new HiveServer.HiveServerHandler();
-		}
-	}
+  public HiveTestService() throws IOException {
+    super();
+    host = "localhost";
+    port = 10000;
+  }
 
-	public void tearDown() throws Exception {
-		super.tearDown();
-		if (standAloneServer) {
-			try {
-				// client.clean();//not in 0.7.X
-				client.shutdown();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			transport.close();
-		}
-	}
+  public void setUp() throws Exception {
+    super.setUp();
+    Path rootDir = getDir(ROOT_DIR);
+    Configuration conf = createJobConf();
+    FileSystem fs = FileSystem.get(conf);
+    fs.delete(rootDir, true);
+    Path metastorePath = new Path("/tmp/metastore_db");
+    fs.delete(metastorePath, true);
+    Path warehouse = new Path("/tmp/warehouse");
+    fs.delete(warehouse, true);
+    fs.mkdirs(warehouse);
 
+    if (standAloneServer) {
+      try {
+        transport = new TSocket(host, port);
+        TProtocol protocol = new TBinaryProtocol(transport);
+        client = new HiveClient(protocol);
+        transport.open();
+      } catch (Throwable e) {
+        e.printStackTrace();
+      }
+    } else {
+      client = new HiveServer.HiveServerHandler();
+    }
+  }
+
+  public void tearDown() throws Exception {
+    super.tearDown();
+    if (standAloneServer) {
+      try {
+        // client.clean();//not in 0.7.X
+        client.shutdown();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+      transport.close();
+    }
+  }
 }
