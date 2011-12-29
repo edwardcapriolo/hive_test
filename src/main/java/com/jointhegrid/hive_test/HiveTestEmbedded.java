79a6f99678093d582373423cc7fc20acc0c4a881
Edward Capriolo <edlinuxguru@gmail.com>
2011-12-29 12:35:02 -0500

    Updated hive to 0.8.0 from MAVEN! Only real adition was hive-builtins and some new exception type

--- src/main/java/com/jointhegrid/hive_test/HiveTestEmbedded.java
+++ src/main/java/com/jointhegrid/hive_test/HiveTestEmbedded.java
@@ -16,9 +16,12 @@ limitations under the License.
 package com.jointhegrid.hive_test;
 
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.CommandNeedRetryException;
 import org.apache.hadoop.hive.ql.Driver;
 import org.apache.hadoop.hive.ql.processors.CommandProcessor;
 import org.apache.hadoop.hive.ql.processors.CommandProcessorFactory;
@@ -35,14 +38,14 @@ public abstract class HiveTestEmbedded extends HiveTestBase {
 
   public void setUp() throws Exception {
     super.setUp();
-    SessionState.initHiveLog4j();
+    //SessionState.initHiveLog4j();
     ss = new SessionState(new HiveConf(HiveTestEmbedded.class));
     SessionState.start(ss);
     c = (HiveConf) ss.getConf();
   }
 
   public int doHiveCommand(String cmd, Configuration h2conf) {
-    int ret = 0;
+    int ret = 40;
     String cmd_trimmed = cmd.trim();
     String[] tokens = cmd_trimmed.split("\\s+");
     String cmd_1 = cmd_trimmed.substring(tokens[0].length()).trim();
@@ -52,9 +55,17 @@ public abstract class HiveTestEmbedded extends HiveTestBase {
     proc = CommandProcessorFactory.get(tokens[0], c);
 
     if (proc instanceof Driver) {
-      ret = proc.run(cmd).getResponseCode();
+      try {
+        ret = proc.run(cmd).getResponseCode();
+      } catch (CommandNeedRetryException ex) {
+        Logger.getLogger(HiveTestEmbedded.class.getName()).log(Level.SEVERE, null, ex);
+      }
     } else {
-      ret = proc.run(cmd_1).getResponseCode();
+      try {
+        ret = proc.run(cmd_1).getResponseCode();
+      } catch (CommandNeedRetryException ex) {
+        Logger.getLogger(HiveTestEmbedded.class.getName()).log(Level.SEVERE, null, ex);
+      }
     }
     return ret;
   }
