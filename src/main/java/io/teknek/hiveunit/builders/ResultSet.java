2db413970dcf3b8db366cbd1e830f09e45174be9
Edward Capriolo <edlinuxguru@gmail.com>
2014-05-29 23:19:11 -0400

    Builder tools

---
+++ src/main/java/io/teknek/hiveunit/builders/ResultSet.java
@@ -0,0 +1,26 @@
+package io.teknek.hiveunit.builders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultSet {
+
+  private List<Row> rows = new ArrayList<Row>();
+  
+  public ResultSet(){
+    
+  }
+  
+  public ResultSet withRow(Row row){
+    rows.add(row);
+    return this;
+  }
+  
+  public List<String> build(){
+    List<String> res = new ArrayList<String>();
+    for (Row r : rows){
+      res.add(r.toString());
+    }
+    return res;
+  }
+}
